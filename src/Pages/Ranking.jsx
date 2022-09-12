af11f708ac84bf2a5466424583499d0edad09100
Ricielli Costa <rici.spfc@gmail.com>
2022-09-12 16:37:58 -0300

    Requisito 18

--- src/Pages/Ranking.jsx
+++ src/Pages/Ranking.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <p data-testid="ranking-title">ranking-title</p>
+        <button
+          data-testid="btn-go-home"
+          type="submit"
+          onClick={ this.handleClick }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
