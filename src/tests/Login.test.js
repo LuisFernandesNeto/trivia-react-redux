f29c9464e57eb3693d042a6a1255b7f0d7788fe2
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-09-12 19:29:05 -0300

    Requisito 4

---
+++ src/tests/Login.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+
+describe('Login de usuários', () => {
+  test('Verifica se a tela home está funcionando completamente', async () => {
+    renderWithRouterAndRedux(<App />);
+    const response = {
+      response_code: 0,
+      response_message: 'Token Generated Successfully!',
+      token: '0ad9ff7a6c9090d4ea225e7467e50e31567bdac8d21a5ed1d774e51033b17c51' };
+
+    const emailInput = screen.getByTestId(/input-gravatar-email/i);
+    const nameInput = screen.getByTestId(/input-player-name/i);
+    expect(emailInput).toBeInTheDocument();
+
+    const playButton = screen.getByRole('button', { name: /Play/i });
+    expect(playButton).toBeInTheDocument();
+    expect(playButton).toBeDisabled();
+
+    userEvent.type(emailInput, 'luisfernandesneto@gmail.com');
+    userEvent.type(nameInput, 'Luis');
+
+    expect(playButton).not.toBeDisabled();
+
+    userEvent.click(playButton);
+
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(response),
+    });
+    expect(global.fetch).toBeCalledTimes(1);
+    await waitFor(() => expect(screen.getByText(/game/i)).toBeInTheDocument());
+
+    /* const { location: { pathname } } = history;
+    expect(pathname).toBe('/'); */
+  });
+});
